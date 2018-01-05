85db38dd9b4c389873c07d59472def6aa0100b0e
NickGatti <nick.gatti@gmail.com>
2018-01-05 13:00:49 -0700

    ch4

--- part1/server.js
+++ part1/server.js
@@ -1,9 +1,27 @@
-var express = require( 'express' );
-var app = express();
-var port = process.env.PORT || 8000;
+const express = require( 'express' );
+const app = express();
+const port = process.env.PORT || 8000;
+const path = require( 'path' )
+
 
 app.get( '/hello', function ( req, res ) {
     res.send( 'Hello' )
+} )
+
+app.get( '/verify/:id', function ( req, res ) {
+    if ( !req.params.id ) {
+        res.sendStatus( 404 );
+    } else if ( isNaN( req.params.id ) ) {
+        res.sendStatus( 404 );
+    } else if ( req.params.id > 13 ) {
+        res.sendStatus( 200 );
+    } else {
+        res.sendStatus( 403 );
+    }
+} )
+
+app.get( '/', function ( req, res ) {
+    res.sendFile( path.join( __dirname, './index.html' ) );
 } )
 
 let createEndpoints = []
