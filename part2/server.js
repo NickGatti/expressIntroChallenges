709d3911c3efa9a1bad7da1b815347b4506297e0
NickGatti <nick.gatti@gmail.com>
2018-01-05 14:32:53 -0700

    part2ch3

--- part2/server.js
+++ part2/server.js
@@ -5,17 +5,29 @@ const port = process.env.PORT || 8000;
 
 app.post( '/create/:name/:age', function ( req, res ) {
     res.send( `You posted ${req.params.name} and ${req.params.age}` );
-    let output = fs.readFileSync( './storage.json', 'utf-8' )
-    output += JSON.stringify( {
+    let output = JSON.parse( fs.readFileSync( './storage.json', 'utf-8' ) )
+    output.push( {
         "name": req.params.name,
         "age": req.params.age
     } )
-    fs.writeFileSync( './storage.json', output )
+    fs.writeFileSync( './storage.json', JSON.stringify( output ) )
 } );
 
 app.get( '/', function ( req, res ) {
     let output = fs.readFileSync( './storage.json', 'utf-8' )
     res.send( output )
+} )
+
+app.get( '/:name', function ( req, res ) {
+    let output = JSON.parse( fs.readFileSync( './storage.json', 'utf-8' ) )
+    for ( var i = 0; i < output.length; i++ ) {
+        output[ i ]
+        if ( req.params.name === output[ i ].name ) {
+            res.send( JSON.stringify( output[ i ] ) )
+            return;
+        }
+    }
+    res.sendStatus( 404 )
 } )
 
 app.use( function ( req, res ) {
