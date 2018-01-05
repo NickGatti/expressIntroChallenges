eda0e14dff6255550c3693a50603c60b6eba0f02
NickGatti <nick.gatti@gmail.com>
2018-01-05 11:49:42 -0700

    ch1

--- part1/server.js
+++ part1/server.js
@@ -1,15 +1,15 @@
-var express = require('express');
+var express = require( 'express' );
 var app = express();
 var port = process.env.PORT || 8000;
 
-app.get('/yourroute', function(req, res) {
-  res.send("stuff");
-});
+app.get( '/hello', function ( req, res ) {
+    res.send( 'Hello' )
+} )
 
-app.use(function(req, res) {
-  res.sendStatus(404);
-});
+app.use( function ( req, res ) {
+    res.sendStatus( 404 );
+} );
 
-app.listen(port, function() {
-  console.log('Listening on port', port);
-});
+app.listen( port, function () {
+    console.log( 'Listening on port', port );
+} );
