5bdb770ee5d7277a6d5825242b0900a1d76a5297
NickGatti <nick.gatti@gmail.com>
2018-01-05 13:38:35 -0700

    part1ch1

--- part2/server.js
+++ part2/server.js
@@ -1,15 +1,22 @@
-var express = require('express');
-var app = express();
-var port = process.env.PORT || 8000;
+const express = require( 'express' );
+const app = express();
+const fs = require( 'fs' )
+const port = process.env.PORT || 8000;
 
-app.get('/yourroute', function(req, res) {
-  res.send("stuff");
-});
+app.post( '/create/:name/:age', function ( req, res ) {
+    res.send( `You posted ${req.params.name} and ${req.params.age}` );
+    let output = fs.readFileSync( './storage.json', 'utf-8' )
+    output += JSON.stringify( {
+        "name": req.params.name,
+        "age": req.params.age
+    } )
+    fs.writeFileSync( './storage.json', output )
+} );
 
-app.use(function(req, res) {
-  res.sendStatus(404);
-});
+app.use( function ( req, res ) {
+    res.sendStatus( 404 );
+} );
 
-app.listen(port, function() {
-  console.log('Listening on port', port);
-});
+app.listen( port, function () {
+    console.log( 'Listening on port', port );
+} );
